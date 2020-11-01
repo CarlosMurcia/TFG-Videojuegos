d594dd9fde9d68604613145f8ce16f1f15b9b802
CarlosMurcia <c.murcia@hotmail.es>
2020-11-01 05:21:12 +0100

    Entrega PEC2

---
+++ DOG ON THE RUN/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpenDoor : MonoBehaviour
+{
+
+  private bool inDoor = false;
+ 
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+      if(collision.gameObject.CompareTag("Player"))
+      {
+          
+          inDoor=true;
+          
+      }
+  }
+ 
+
+    private void Update()
+    {
+       if(inDoor)
+       {
+            SceneManager.LoadScene ("End");
+       } 
+    }
+}
---
+++ DOG ON THE RUN/Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+	
+    
+    public GameObject optionsPanel;
+
+    public void OptionsPanel()
+    {
+       
+        Time.timeScale=0;
+        optionsPanel.SetActive(true);
+    
+    }
+    public void Continue()
+    {
+        Time.timeScale=1;
+        optionsPanel.SetActive(false);
+    }
+    public void OtherOptions()
+    {
+        
+    }
+    public void Exit()
+    { 
+        SceneManager.LoadScene("MainMenu");
+    }
+     public void Update()
+    {
+        if (Input.GetKey("escape"))
+	{
+        Time.timeScale=0;
+        optionsPanel.SetActive(true);
+    }
+    }
+    }
